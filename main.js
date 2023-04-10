8a7e4c773f0967349a031eefc0bb1648f34d9532
Down The Shed <130371769+Down-The-Shed@users.noreply.github.com>
2023-04-10 17:30:54 +0100

    added mobile support

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+// const mediaQuery = '(max-width: 400px)';
+// const mediaQueryList = window.matchMedia(mediaQuery); //this checks for resizes
+const mediaQueryList = window.matchMedia('(max-width: 400px)'); //this checks once
+
+var sbOpenWidth = 250; //String(sbOpenWidth) + "px"
+var sbClosedWidth = 50; //String(sbClosedWidth) + "px"
+
+//for checking for resizes
+// mediaQueryList.addEventListener('change', event => {
+//   if (event.matches) {
+//     console.log('The window is now 400px or under');
+//     sbClosedWidth = 100;
+//     sbOpenWidth = 500;
+//   } else {
+//     console.log('The window is now over 400px');
+//     sbClosedWidth = 50;
+//     sbOpenWidth = 250;
+//   }
+// })
+
+//for checking once
+  if (mediaQueryList.matches) {
+    console.log('The window is now 400px or under');
+    sbClosedWidth = 100;
+    sbOpenWidth = 500;
+    
+    sleep(500).then(() => {
+        toggleNav();
+        toggleNav();
+        document.getElementById("menubtn").classList.toggle('SmallScreen');
+        document.getElementById("SideNavigation").classList.toggle('SmallScreen');
+    });
+  }
+
 function toggleMenuBtn(x) {
     x.classList.toggle("change");
 }
@@ -8,18 +42,20 @@ function toggleNav() {
     links = document.getElementById("Links")
     // console.log(links.style);
     
-    //close if open
-    if (sidenav.style.width == "250px") {
-        sidenav.style.width = "50px";
-        main.style.marginLeft = "50px";
-        links.style.left = "-125px";
-        document.body.style.backgroundColor = "white";
-    } else { //open if is collapsed or on error
-        sidenav.style.width = "250px";
-        main.style.marginLeft = "250px";
-        links.style.left = "50px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    } 
+    //if using device with small screen use a larger sidebar
+
+        //close if open
+        if (sidenav.style.width == String(sbOpenWidth) + "px") {
+            sidenav.style.width = String(sbClosedWidth) + "px";
+            main.style.marginLeft = String(sbClosedWidth) + "px";
+            links.style.left = "-" + String(sbOpenWidth/2) + "px";
+            document.body.style.backgroundColor = "white";
+        } else { //open if is collapsed or on error
+            sidenav.style.width = String(sbOpenWidth) + "px";
+            main.style.marginLeft = String(sbOpenWidth) + "px";
+            links.style.left = String(sbClosedWidth) + "px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        } 
 }
 function sleep (time) {
     return new Promise((resolve) => setTimeout(resolve, time));
@@ -34,7 +70,7 @@ function UpdateIFrame(){
         // Do something after the sleep!
     
         // Selecting the iframe element
-        var iframe = document.getElementById("displayFrame");
+        var iframe = document.getElementById("displayframe");
         // Adjusting the iframe height
         iframe.style.height = iframe.contentWindow.document.body.scrollHeight + 10 + 'px';
     });
