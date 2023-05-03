56ec3d543bc557ecd2bddfeba9394c846041c3a8
Down The Shed <130371769+Down-The-Shed@users.noreply.github.com>
2023-05-03 12:58:49 +0100

    added more pictures
    I added a few more pictures and also corrected some of the words used on the 'about me' page.

--- gallery.js
+++ gallery.js
@@ -1,25 +1,26 @@
-function Loadimgs() {
-    var image_div = document.getElementById("Images");
-    var file_path = "images/";
-    var ext = ".jpg";
-    var files = [];
+window.onload = function(){ 
+// Get the modal
+var modal = document.getElementById("myModal");
 
-    for (var m = 0; m < 20; m++) {
-        try {
-        files[m] = file_path + m + ext;
-        // console.log(m);
-        } catch (e) {
-        // exit the loop
-        break; 
-        }
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var images = document.getElementsByClassName("ClickableImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+    var img = images[i];
+    // and attach our click listener for this image.
+    img.onclick = function(evt) {
+      console.log(evt);
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
     }
-
-
-    for (let i = 0; i < files.length; i++) {
-        var img = document.createElement('img');
-        img.src = files[i];
-        img.height = 256;
-        img.loading = "lazy";
-        image_div.appendChild(img);
-    }
-}
+  }
+  
+  var span = document.getElementById("close");
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+};
